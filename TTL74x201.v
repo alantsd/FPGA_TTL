e99bf132feff35769acce1d88397b6f40030b757
alantsd <alantsd@hotmail.com>
2025-12-24 16:01:17 +0800

    bug fix TTL74x201.v

--- TTL74x201.v
+++ TTL74x201.v
@@ -6,8 +6,8 @@
 //	 ADDR_WIDTH - number of address bits (default 8 for 256 words)
 //	 DATA_WIDTH - width of each word (default 1 bit)
 // Behavior:
-//	 - Write: when CE=0, WE=0, data_in is stored at address
-//	 - Read:  when CE=0, OE=0, data_out drives stored value
+//	 - Write: when select_n low and R_W low,  data_in is stored at address
+//	 - Read:  when select_n low and R_W high, Q_n drives compliment of stored value
 //	 - Tri-state: when OE=1 or CE=1, data_out is high-Z (not supported)
 //------------------------------------------------------------
 module TTL74x201 
@@ -16,26 +16,31 @@ module TTL74x201
 	parameter DATA_WIDTH = 1
 )
 (
-	input  wire					 R_W,		// Read == 1, Write enable (active low)
+	input  wire					 clk,		// added clock for FPGA needs
+	input  wire					 R_W,		// Read == 1, Write == 0
 	input  wire					 S1_n,		// select 1
 	input  wire					 S2_n,		// select 2
 	input  wire					 S3_n,		// select 3
 	input  wire [ADDR_WIDTH-1:0] A,			// Address input
 	input  wire [DATA_WIDTH-1:0] D,			// Data input
-	output wire [DATA_WIDTH-1:0] Q_n		// Data output (tri-state)
+	output wire [DATA_WIDTH-1:0] Q_n		// Data output (tri-state not supported)
 );
+	wire select_n;
+	assign select_n = S1_n | S2_n | S3_n;
+
 	reg [DATA_WIDTH-1:0] mem [0:(1<<ADDR_WIDTH)-1];
 
-	wire select_n;
-	assign select_n = S1_n & S2_n & S_n;
+	always @(posedge clk)
+		if(!select_n && !R_W)
+			mem[A] <= D; // Write operation
 
-	wire clk;
-	assign clk = !select_n && !R_W;
+	reg [DATA_WIDTH-1:0] out;
 
 	always @(posedge clk)
-		mem[A] <= D; // Write operation
+		if(!select_n && R_W)
+			out <= ~mem[A]; // Read operation
 
 	// no support for tri-state output, always read 
-	assign Q_n = ~mem[A];
+	assign Q_n = out;
 
 endmodule
