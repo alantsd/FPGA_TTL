1f9db7d7d9bb9c2c675649b47ebcd2eaf8c7f637
Alan Sing Teik <alantsd@hotmail.com>
2026-01-15 00:36:56 +0800

    8-bit identity comparator with Enable, output low

--- TTL74x521.v
+++ TTL74x521.v
@@ -1,18 +1,18 @@
-// TTL74x521 8-bit identity comparator (combinational)
-// Purpose: Behavioral, Parameterized model of SN74ALS521 (8-bit identity comparator)
+// TTL74x521.v
+// Purpose: Behavioral, Parameterized model of SN74ALS521 (8-bit identity comparator with Enable, output low)
 // Author: Alan Sing Teik
 // Parameters:
 //	 WIDTH	- default 8
 
 module TTL74x521
 #(
-	parameter integer WIDTH	= 8,
+	parameter WIDTH	= 8
 )
 (
 	input  wire [WIDTH-1:0] P,
 	input  wire [WIDTH-1:0] Q,
 	input  wire				G_n,	// enable (ACTIVE_LOW)
-	output wire				P_n		// low when P == Q and G_n == low
+	output wire				EQ_n	// low when P == Q and G_n == low
 );
 
 	// XOR each-bit
@@ -23,6 +23,6 @@ module TTL74x521
 	wire neq;
 	assign neq = (|diff);
 
-	assign P_n = neq | G_n;
+	assign EQ_n = neq | G_n;
 
 endmodule
